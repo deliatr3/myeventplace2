f6d21ac000381557b602953a802b3d3de1ebc82d
deliatr3 <troncdel@students.zhaw.ch>
2025-05-29 13:11:50 +0200

    neu

--- src/routes/veranstalter/+page.server.js
+++
@@ -1,9 +0,0 @@
-import db from '$lib/db.js';
-
-
-export async function load() {
-	const organizers = await db.getOrganizers();
-	return { organizers }; // funktioniert, wenn `$props()` korrekt verwendet wird
-}
-
-
